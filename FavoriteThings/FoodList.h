762a372af0223b7e807ae672b58b55d3b246d8d5
King Pho <King Pho@BRYAN>
2018-04-13 12:52:44 -0400

    new commit

--- FavoriteThings/FoodList.h
+++ FavoriteThings/FoodList.h
@@ -9,11 +9,14 @@ using namespace std;
 class FoodList
 {
 private:
-	FoodItem fList[1000];
+	FoodItem fList[100];
 	int listSize;
 public:
+//created a constructor for FoodList
 	FoodList();
+//created function that adds item to the list
 	int addFoodItem();
+//prints the list
 	void printList();
 
 
